5445ae97d266f646143f1d0ab4c90d9b04f2fec0
Pardis Zahraei <paradisez2001@gmail.com>
2021-05-04 20:15:50 +0430

    AI, Card

--- src/main/java/model/AI.java
+++ src/main/java/model/AI.java
@@ -1,4 +1,47 @@
 package model;
 
 public class AI {
+    private Player AIPlayer;
+
+    public Player getAIPlayer() {
+        return AIPlayer;
+    }
+
+    public AI(Player AIPlayer) {
+        this.AIPlayer = AIPlayer;
+    }
+
+    public void setAIPlayer(Player AIPlayer) {
+        this.AIPlayer = AIPlayer;
+    }
+
+    public void setTrap()
+    {
+
+    }
+
+    public void activateSpell()
+    {
+
+    }
+
+    public void SummonMonsterWithHighestLevel()
+    {
+
+    }
+
+    public void AttackMonsterwithChances()
+    {
+
+    }
+
+    public void sacraficeTheWeak()
+    {
+
+    }
+
+    public void defendCards()
+    {
+
+    }
 }
--- src/main/java/model/Card.java
+++ src/main/java/model/Card.java
@@ -21,4 +21,5 @@ public class Card {
         }
         return false;
     }
+
 }
