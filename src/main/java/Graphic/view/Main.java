5dbf8a42583e7a01893c28ff0695c23fc8968900
bhrdbn <bahardib5@gmail.com>
2021-06-09 03:44:42 +0430

    graphic copy

---
+++ src/main/java/Graphic/view/Main.java
@@ -0,0 +1,12 @@
+package view;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static Scanner scanner=new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Login.getInstance().run();
+    }
+}
