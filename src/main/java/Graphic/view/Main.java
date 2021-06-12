945f6145dc299df0c61f592b18f97d71e409bb46
Pardis Zahraei <paradisez2001@gmail.com>
2021-06-12 13:39:41 +0430

    graphic Mainmenu

--- src/main/java/Graphic/view/Main.java
+++ src/main/java/Graphic/view/Main.java
@@ -1,10 +1,29 @@
 package Graphic.view;
 
 
-import javafx.*;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 import java.util.Scanner;
 
-public class Main  {
+public class Main extends Application {
+
+    public static Stage stage;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        stage = primaryStage;
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        primaryStage.setTitle("Login");
+        primaryStage.setScene(new Scene(root, 1000, 760));
+        primaryStage.show();
+
+    }
+
 
     public static Scanner scanner=new Scanner(System.in);
 
@@ -13,5 +32,3 @@ public class Main  {
         Login.getInstance().run();
     }
 }
-
-
