648391b72ff611b31f937584a12004be836fc24b
Pardis Zahraei <paradisez2001@gmail.com>
2021-06-26 11:11:30 +0430

    edit

--- src/main/java/model/ActionMonster.java
+++ src/main/java/model/ActionMonster.java
@@ -3,55 +3,188 @@ package model;
 import java.util.ArrayList;
 
 public class ActionMonster {
-    public void setAction(int cardControllerNumber, MonsterCard monsterCard, Deck deck,MonsterCard monsterCard1,MonsterCard monsterCard2) {
-        if (cardControllerNumber == 1) {
-            increaseAttackAll(monsterCard);
-        }  if (cardControllerNumber == 2) {
-            invinsibleTillOtherMonstersExist(monsterCard, deck);
-        }  if (cardControllerNumber == 3) {
-            attack(monsterCard);
-        }  if (cardControllerNumber == 4) {
-            defence(monsterCard);
-        }  if (cardControllerNumber == 5) {
-            ifFaceupcanuseOnce(monsterCard);
-        }  if (cardControllerNumber == 6) {
-            upperDefence(monsterCard);
-        }  if (cardControllerNumber == 7) {
-            upperAttack(monsterCard);
-        }  if (cardControllerNumber == 8) {
-            sendFromsideDECK(monsterCard, deck);
-        }  if (cardControllerNumber == 9) {
-            getInBoardWithSacrifices(monsterCard, deck);
-        }  if (cardControllerNumber == 10) {
-            getInBoardWithSacrifices2(monsterCard,monsterCard2,monsterCard1, deck);
-        }  if (cardControllerNumber == 11) {
-            canNotBeAttacked(monsterCard);
-        } if (cardControllerNumber == 12) {
-            getInBoardWithSacrifices3(monsterCard,deck);
+    public void setAction(int cardControllerNumber, MonsterCard monsterCard, Deck deck,  PlayBoard playBoard, ArrayList<MonsterCard> monsterCards,int pos) {
+    if (cardControllerNumber==1)
+    {
+        increaseAttackAll(monsterCards,monsterCard);
+    }
+    if (cardControllerNumber==2)
+    {
+        canNotBeAttacked(monsterCard);
+    }
+    if (cardControllerNumber==3)
+    {
+        getInBoardWithSacrifices(monsterCard,deck);
+    }
+    if (cardControllerNumber==4)
+    {
+        getInBoardWithSacrifices2(monsterCards,deck);
+    }
+    if (cardControllerNumber==5)
+    {
+        getInBoardWithSacrifices3(monsterCard,deck);
+    }
+    if (cardControllerNumber==6)
+    {
+        diewhenattack(monsterCard,playBoard);
+    }
+    if (cardControllerNumber==7)
+    {
+        acardwhenturned(monsterCard,playBoard);
+    }
+    if (cardControllerNumber==8)
+    {barbaroswithoutsac(monsterCards,monsterCard,deck,pos);}
+    if (cardControllerNumber==9)
+    {barbaroswitsac(monsterCards,monsterCard,deck,pos);}
+    if (cardControllerNumber==10)
+    {
+        calculate(monsterCards,monsterCard);
+    }
+    if (cardControllerNumber==11)
+    {
+        sacraficeOneToget(playBoard,deck,pos,monsterCards);
+    }
+
+
+
+
+    }
+// 1  COMMANDKNIGHT
+    // 2 SUIJIN
+    //3 CRABTURTLE
+    // 4 GATE GUARDIAN
+    //5 skull guardian
+    //6 yomi ship
+    //7 man eater bug
+    //8 9 king barbaros
+    //10 calculator
+//11 herald of creation
+    public void increaseAttackAll(ArrayList<MonsterCard> monsterCards,MonsterCard monsterCard) //1
+    {
+        for (MonsterCard monsterCardd : monsterCards) {
+            monsterCardd.setAttack(monsterCardd.getAttack() + 400);
+
+        }
+        if (monsterCards.size() < 2)
+            monsterCard.setIsAttack(false);
+        else
+            monsterCard.setIsAttack(true);
+    }
+
+
+    public void getInBoardWithSacrifices(MonsterCard monsterCard, Deck deck) //3
+    {
+        MonsterCard monsterCard1 = new MonsterCard("CrabTurtle", 1, "Ritual", "description:This monster can only be Ritual Summoned with the Ritual Spell Card, \"Turtle Oath\". You must also offer monsters whose total Level Stars equal 8 or more as a Tribute from the field or your hand.", true, 10200, 2550, 2500, null, "WATER", 8, "Aqua");
+
+        deck.removeCard(monsterCard, 1);
+        deck.addCard(monsterCard1, 1);
+    }
+
+    public void getInBoardWithSacrifices2(ArrayList<MonsterCard> monsterCards, Deck deck) //4
+    {
+        MonsterCard monsterCard1 = new MonsterCard("GateGuardian", 1, "Ritual", "description:This monster can only be Ritual Summoned with the Ritual Spell Card, \"Turtle Oath\". You must also offer monsters whose total Level Stars equal 8 or more as a Tribute from the field or your hand.", true, 10200, 2550, 2500, null, "WATER", 8, "Aqua");
+        if (monsterCards.size() > 3) {
+            monsterCards.remove(1);
+            monsterCards.remove(2);
+            monsterCards.remove(3);
+            deck.addCard(monsterCard1, 1);
+            monsterCards.add(monsterCard1);
+        } else {
+            System.out.println("you cant summon");
         }
 
-
     }
-// 1 ,2 COMMANDKNIGHT
-    // 12 SUIJIN
-    //10 CRABTURTLE
-    // 25 GATE GUARDIAN
-    //26 skull guardian
 
-    public void increaseAttackAll(MonsterCard monsterCard) //1
+    public void getInBoardWithSacrifices3(MonsterCard monsterCard, Deck deck) //5
     {
-        monsterCard.setAttack(monsterCard.getAttack()+400);
+        MonsterCard monsterCard1 = new MonsterCard("SkullGuardian", 1, "Ritual", "description This monster can only be Ritual Summoned with the Ritual Spell Card, \"Novox's Prayer\". You must also offer monsters whose total Level Stars equal 7 or more as a Tribute from the field or your hand.",
+                true, 7900, 2050, 2500, null, "LIGHT", 7, "Warrior");
+
+        deck.removeCard(monsterCard, 1);
+
+        deck.addCard(monsterCard1, 1);
     }
 
-    public void invinsibleTillOtherMonstersExist(MonsterCard monsterCard, Deck deck) //2
+    public void canNotBeAttacked(MonsterCard monsterCard) //2
     {
-        if (deck.getMonsters(1).size() + deck.getMonsters(2).size() + deck.getMonsters(3).size() > 1)
-            monsterCard.setDefence(5000);
+        monsterCard.setIsAttack(false);
     }
 
+    public void diewhenattack(MonsterCard monsterCard, PlayBoard playBoard) //6
+    {
+        if (monsterCard.getAttack() < playBoard.getSelectedMonsterCard().getAttack()) {
+            if (monsterCard.getDefence() == 0) {
+                playBoard.getSelectedMonsterCard().setDefence(0);
+            }
+        }
+    }
+
+    public void acardwhenturned(MonsterCard monsterCard, PlayBoard playBoard) //7
+    {
+        monsterCard.setSide(true);
+        playBoard.getAttackerCards().remove(2);
+    }
+
+
+    public void barbaroswithoutsac(ArrayList<MonsterCard> monsterCards, MonsterCard monsterCard, Deck deck, int pos) //8
+    {
+        monsterCard.setAttack(1900);
+        deck.addCard(monsterCard, pos);
+
+    }
+
+    public void barbaroswitsac(ArrayList<MonsterCard> monsterCards, MonsterCard monsterCard, Deck deck, int pos) //9
+    {
+        if (monsterCards.size() >= 3) {
+            monsterCards.remove(1);
+            monsterCards.remove(2);
+            monsterCards.remove(3);
+        } else {
+            System.out.println("you cant summon this monster");
+        }
+        monsterCards.add(monsterCard);
+        deck.addCard(monsterCard, 1);
+
+    }
+
+    public void calculate(ArrayList<MonsterCard> monsterCards, MonsterCard monsterCard) //10
+    {
+        int sum = 0;
+        for (MonsterCard monsterCardd : monsterCards) {
+            sum = sum + monsterCardd.getAttack();
+        }
+        sum = sum * 300;
+        monsterCard.setAttack(sum);
+    }
+
+
+    public void sacraficeOneToget(PlayBoard playBoard, Deck deck, int num, ArrayList<MonsterCard> monsterCards) //11
+    {
+        int i = 0,m=0;
+        String t = "";
+        deck.getMainDeck().remove(num);
+        for (MonsterCard monsterCardd : monsterCards) {
+
+            if (monsterCardd.getLevel() > i)
+            {i = monsterCardd.getLevel();
+            t.equals(monsterCardd.name);
+            }
+
+        }
+        for (int j = 0; j < monsterCards.size(); j++) {
+            if (t.equals(monsterCards.get(j).getName()))
+            {
+                m=j;
+            }
+        }
+        playBoard.getGraveyards().get(m);
+    }
+
+
+/*
     public void attack(MonsterCard monsterCard) //3
     {
-        monsterCard.setAttack(monsterCard.getAttack()+1900);
+        monsterCard.setAttack(monsterCard.getAttack() + 1900);
     }
 
     public void defence(MonsterCard monsterCard) //4
@@ -62,7 +195,8 @@ public class ActionMonster {
 
     public void ifFaceupcanuseOnce(MonsterCard monsterCard) //5
     {
-        monsterCard.setAttack(10000);
+        if (monsterCard.isSide())
+            monsterCard.setAttack(monsterCard.getAttack() + 500);
         monsterCard.setSide(false);
 
     }
@@ -86,85 +220,5 @@ public class ActionMonster {
     public void run() {
 
     }
-
-
-    public void getInBoardWithSacrifices(MonsterCard monsterCard, Deck deck) //10
-    {
-        MonsterCard monsterCard1 = new MonsterCard("CrabTurtle", 1, "Ritual", "description:This monster can only be Ritual Summoned with the Ritual Spell Card, \"Turtle Oath\". You must also offer monsters whose total Level Stars equal 8 or more as a Tribute from the field or your hand.", true, 10200, 2550, 2500, null, "WATER", 8, "Aqua");
-
-        deck.removeCard(monsterCard, 1);
-        deck.addCard(monsterCard1, 1);
-    }
-
-    public void getInBoardWithSacrifices2(MonsterCard monsterCard,MonsterCard monsterCard2,MonsterCard monsterCard3 ,Deck deck) //25
-    {
-        MonsterCard monsterCard1 = new MonsterCard("GateGuardian", 1, "Ritual", "description:This monster can only be Ritual Summoned with the Ritual Spell Card, \"Turtle Oath\". You must also offer monsters whose total Level Stars equal 8 or more as a Tribute from the field or your hand.", true, 10200, 2550, 2500, null, "WATER", 8, "Aqua");
-
-        deck.removeCard(monsterCard, 1);
-        deck.removeCard(monsterCard2, 2);
-        deck.removeCard(monsterCard3, 3);
-        deck.addCard(monsterCard1, 1);
-    }
-    public void getInBoardWithSacrifices3(MonsterCard monsterCard,Deck deck) //25
-    {
-        MonsterCard monsterCard1 = new MonsterCard("SkullGuardian", 1, "Ritual", "description This monster can only be Ritual Summoned with the Ritual Spell Card, \"Novox's Prayer\". You must also offer monsters whose total Level Stars equal 7 or more as a Tribute from the field or your hand.",
- true, 7900, 2050, 2500, null, "LIGHT", 7, "Warrior");
-
-        deck.removeCard(monsterCard, 1);
-
-        deck.addCard(monsterCard1, 1);
-    }
-
-    public void canNotBeAttacked(MonsterCard monsterCard) //12
-    {
-        monsterCard.setIsAttack(false);
-    }
-/*
-    public void decreaseAttackWithOutSacrifices(int cardControllerNumber) //13
-    {
-
-    }
-
-    public void destroyAllCards(int cardControllerNumber) //14
-    {
-
-    }
-
-    public void summonCyberseOnly(int cardControllerNumber) //15
-    {
-
-    }
-
-    public void attackIStheSumOfFrontCards(int cardControllerNumber) //16
-    {
-
-    }
-
-    public void noTrapsAvailableForOpponent(int cardControllerNumber) //17
-    {
-
-    }
-
-    public void loseOnegetFromGraveYard(int cardControllerNumber) //18
-    {
-
-    }
-
-    public void destroysBothCardsWithoutAnyLifeDamage(int cardControllerNumber) //19
-    {
-
-    }
-
-    public void getAnotherDefenceStateCard(int cardControllerNumber) //20
-    {
-
-    }
-
-    public void sacraficeOneToget(int cardControllerNumber) //21
-    {
-
-    }
-
- */
-
+*/
 }
