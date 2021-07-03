5a45aab997a9b5a5534aad4cc3be264eb89080e4
Pardis Zahraei <paradisez2001@gmail.com>
2021-07-03 14:30:26 +0430

    edit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,11 @@ module projectap2021 {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.media;
+    requires org.junit.jupiter.api;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires java.desktop;
+    requires com.google.gson;
     opens Graphic to javafx.fxml;
     exports Graphic.view;
 
