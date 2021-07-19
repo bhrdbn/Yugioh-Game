8b2d466365bb1ff8b97c433782a342dd99f4dd4f
Pardis Zahraei <paradisez2001@gmail.com>
2021-07-19 20:07:41 +0430

    edit

---
+++ src/main/java/sample/Controller.java
@@ -0,0 +1,21 @@
+package sample;
+
+import javafx.scene.input.MouseEvent;
+
+public class Controller {
+
+    public void liveGameShow(MouseEvent mouseEvent) {
+    }
+
+    public void Rating(MouseEvent mouseEvent) {
+    }
+
+    public void TopGames(MouseEvent mouseEvent) {
+    }
+
+    public void rewatchPastGames(MouseEvent mouseEvent) {
+    }
+
+    public void Back(MouseEvent mouseEvent) {
+    }
+}
